c6e4e0d627647cf0ae2f094c7fb92da1cde9e327
Gunter <kaiz@yandex.ru>
2022-07-05 19:17:21 +0300

    seminar 3 end

--- Program.cs
+++ Program.cs
@@ -1,48 +1,102 @@
 ﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-string number = Console.ReadLine();
-Console.WriteLine();
+// string number = Console.ReadLine();
+// Console.WriteLine("enter a number: ");
 
 
-{
-    if (number[0] == number[4] || number[1] == number[3])
-    {
-        Console.WriteLine($"your number: {number} is palindrome");
-    }
-    else
-    {
-        Console.WriteLine($"your number: {number} is not a palindrome");
-    }
-}
+// {
+//     if (number[0] == number[4] && number[1] == number[3])
+//     {
+//         Console.WriteLine($"your number: {number} is palindrome");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"your number: {number} is not a palindrome");
+//     }
+// }
 
 
 
 
 // Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-// double ShowDistance (double x1, double y1, double z1, double x2, double y2, double z2)
+// double ShowDistance(double x1, double x2, double y1, double y2, double z1, double z2)
 // {
-// double lineX = x2-x1;
-// double lineY = y2-y1;
-// double lineZ = z2-z1;
+//     double lineX = x2 - x1;
+//     double lineY = y2 - y1;
+//     double lineZ = z2 - z1;
 
-// double DistQuad = lineX*lineX + lineY*lineY + lineZ*lineZ;
-// double result = Math.Sqrt(DistQuad); 
-
-// return result;
+//     double DistQuad = lineX*lineX + lineY*lineY + lineZ*lineZ;
+//     double result = Math.Sqrt(DistQuad);
+//     return result;
 // }
 
-// Console.WriteLine("enter x coordinate: ");
-// lineX = Convert.ToInt32(Console.ReadLine());
 
-// Console.WriteLine("enter y coordinate: ");
-// lineY = Convert.ToInt32(Console.ReadLine());
+// Console.Write("enter x coordinate: "); 
+// int x0 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("enter y coordinate: ");
+// int y0 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("enter z coordinate: ");  
+// int z0 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("distance between two points in 3d is: " + ShowDistance);
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+// Не выполняется программа. Дает ввести координаты и выдает результат, на такой: 
+// distance between two points in 3d is System.Func`7[System.Double,System.Double,System.Double,System.Double,System.Double,System.Double,System.Double]
+// -----------------------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// void Cube (int N)
+//    {
+//     Console.Write("imput number: ");
+//     int x = Convert.ToInt32(Console.ReadLine());
+//     int y; 
+//     int count = 0;
+//     int N = 1;
+    
+//     while (count<x)
+//     {
+//       y=N*N*N;
+//       Console.Write(y);
+//       N++;
+//       count++;
+//     }
+//    }
 
 
-// Console.WriteLine("enter z coordinate: ");
-// lineZ = Convert.ToInt32(Console.ReadLine());
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+// Код по последней задаче вообще не работает. Не могу понять почему, но всё равно отправлю вам, может быть дадите советы.
+// Думаю, что мне нужны дополнительные занятия. Чем дальше, тем хуже понимаю. На каждую задачу уходит очень много времени,
+// а должно быть наоборот. Не подскажете, по вопросу доп. занятий мне лучше в гикбрейнс обратиться?
+// -----------------------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------------------
 
-// ShowDistance(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
